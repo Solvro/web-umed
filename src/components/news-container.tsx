9b66446cde4edc284935bacfa77f43271e4bad09
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-04-13 15:23:52 +0200

    feat: add news

---
+++ src/components/news-container.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { fetchData } from "../lib/api";
+import { NewsPost } from "../lib/types";
+import { NewsCard } from "./news-card";
+
+export async function NewsContainer() {
+  let posts;
+
+  try {
+    const response = await fetchData<{ data: NewsPost[] }>("items/news");
+    posts = response.data;
+  } catch (error) {
+    console.error("Error fetching news posts:", error);
+    return null;
+  }
+
+  if (posts.length === 0) {
+    return <h1>Pustka</h1>;
+  }
+
+  return (
+    <div className="flex w-2/3 flex-col gap-3">
+      {posts.map((post) => (
+        <NewsCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
