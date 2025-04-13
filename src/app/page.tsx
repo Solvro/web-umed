733239491b4fa838d8d5394c6e39a9a7ba55817e
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-04-13 16:18:44 +0200

    fix: add card as component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { NewsContainer } from "@/components/news-container";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-10 overflow-hidden bg-gradient-to-br from-slate-950 to-blue-900">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 overflow-hidden bg-gradient-to-br from-slate-950 to-blue-900 p-8">
       <NewsContainer />
     </div>
   );
