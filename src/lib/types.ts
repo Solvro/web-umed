4700cd21e618cd94882c7a0c2737909da86866a6
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-04-13 15:23:52 +0200

    feat: add news

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,13 @@
 /* eslint-disable unicorn/no-empty-file, import/consistent-type-specifier-style */
 // Here implement the types used in the app
+
+export interface NewsPost {
+  id: string;
+  status: string;
+  sort: number;
+  userCreated: string;
+  dateCreated: string;
+  userUpdated: string;
+  dateUptated: string;
+  content: string;
+}
