112015c2e055a3cae0d344dec14793086d0e302b
TeraWattHour <65347730+TeraWattHour@users.noreply.github.com>
2025-04-14 18:12:23 +0200

    fix: static footer entries, include footer in the layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,51 +1,30 @@
-/* eslint-disable react/no-array-index-key */
-// Working with immutable data in this file so index as keys is fine.
 import Link from "next/link";
 import type { ReactNode } from "react";
-
-// TODO: provide actual values
-const ELEMENTS = {
-  Kontakt: [
-    <FooterLink key="contact" href="mailto:john.doe@pwr.edu.pl">
-      john.doe@pwr.edu.pl
-    </FooterLink>,
-  ],
-  "Przydatne linki": [
-    <FooterLink key="home" href="/">
-      Strona główna
-    </FooterLink>,
-    <FooterLink key="reports" href="/#">
-      Zgłaszanie problemów
-    </FooterLink>,
-  ],
-  "Social media": [
-    <FooterLink key="facebook" href="https://facebook.com/#">
-      Facebook
-    </FooterLink>,
-    <FooterLink key="instagram" href="https://instagram.com/#">
-      Instagram
-    </FooterLink>,
-  ],
-};
 
 export function Footer() {
   return (
     <footer className="text-primary-foreground mt-32 w-full">
       <div className="mx-auto mb-14 grid w-full max-w-screen-xl grid-cols-1 gap-4 px-4 sm:grid-cols-2 lg:grid-cols-4">
-        {Object.entries(ELEMENTS).map(([title, entries]) => (
-          <div key={title} className="mb-4">
-            <div className="mb-3">
-              <span className="text-lg font-medium uppercase">{title}</span>
-              <div className="from-primary-foreground/60 h-0.5 w-32 bg-gradient-to-r to-transparent"></div>
-            </div>
-            <ul>
-              {entries.map((entry, index) => (
-                <li key={index}>{entry}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        <FooterSection title="Kontakt">
+          <FooterLink href="mailto:john.doe@pwr.edu.pl">
+            john.doe@pwr.edu.pl
+          </FooterLink>
+        </FooterSection>
+
+        <FooterSection title="Przydatne linki">
+          <FooterLink href="/">Strona główna</FooterLink>
+          <FooterLink href="/#">Zgłaszanie problemów</FooterLink>
+        </FooterSection>
+        <FooterSection title="Social media">
+          <FooterLink href="https://facebook.com/#" target="_blank">
+            Facebook
+          </FooterLink>
+          <FooterLink href="https://instagram.com/#" target="_blank">
+            Instagram
+          </FooterLink>
+        </FooterSection>
       </div>
+
       <div className="bg-primary-foreground/10">
         <div className="mx-auto flex max-w-screen-xl flex-row items-center justify-between px-4 py-6">
           <span className="opacity-80 select-none">
@@ -60,9 +39,27 @@ export function Footer() {
   );
 }
 
+function FooterSection({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) {
+  return (
+    <div className="mb-4">
+      <div className="mb-3">
+        <span className="text-lg font-medium uppercase">{title}</span>
+        <div className="from-primary-foreground/60 h-0.5 w-32 bg-gradient-to-r to-transparent"></div>
+      </div>
+      <ul className="flex flex-col">{children}</ul>
+    </div>
+  );
+}
+
 function FooterLink({
   href,
-  target = "_blank",
+  target,
   children,
   label,
 }: {
