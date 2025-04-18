0fb51017389872b1de01340602719ffb311bc2aa
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-04-18 11:13:38 +0200

    fix: remove fk comment

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,3 @@
-/* eslint-disable unicorn/no-empty-file, import/consistent-type-specifier-style */
-// Here implement the types used in the app
-
 export interface NewsPost {
   id: string;
   title: string;
