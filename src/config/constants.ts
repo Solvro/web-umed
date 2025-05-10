ce0d9a941009729e8789b463910a39e9dd0bf175
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2025-05-10 13:57:22 +0200

    fix: handle errors while sending feedback and replace link with BugFormButton

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -18,10 +18,11 @@ export const MUTATION_KEYS = {
 };
 
 export const QUERY_KEYS = {
-  EMAIL: "entry.280273623",
-  TITLE: "entry.1248470767",
-  CONTENT: "entry.881414235",
+  EMAIL: process.env.BUG_FORM_EMAIL_KEY ?? "entry.280273623",
+  TITLE: process.env.BUG_FORM_TITLE_KEY ?? "entry.1248470767",
+  CONTENT: process.env.BUG_FORM_CONTENT_KEY ?? "entry.881414235",
 };
 
 export const REPORT_FORM_URL =
+  process.env.BUG_FORM_URL ??
   "https://docs.google.com/forms/u/0/d/e/1FAIpQLSd2xwpWLh8fPizL-ik_uSEOmppCPCRiF87Wu0vli1ElLurVYA/formResponse";
