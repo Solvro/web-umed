7e9265c31223a4746b2be26568e0099188db18ec
Konrad Guzek <konrad@guzek.uk>
2025-07-23 21:10:34 +0200

    fix: news section width

--- src/components/homepage/news-section.tsx
+++ src/components/homepage/news-section.tsx
@@ -4,9 +4,9 @@ import { Quaver } from "../notes";
 export function NewsSection() {
   return (
     <div className="bg-faded-gradient flex items-center justify-center shadow-xl lg:pt-30">
-      <div className="relative">
-        <div className="bg-primary flex w-full flex-col p-12 sm:[clip-path:ellipse(95%_100%_at_50%_100%)] lg:px-20 lg:[clip-path:ellipse(70%_100%_at_50%_100%)]">
-          <h1 className="text-primary-foreground mt-10 mb-20 text-center text-4xl lg:mt-0">
+      <div className="relative w-full">
+        <div className="bg-primary flex flex-col p-12 sm:[clip-path:ellipse(95%_100%_at_50%_100%)] lg:px-20 lg:[clip-path:ellipse(70%_100%_at_50%_100%)]">
+          <h1 className="text-primary-foreground mt-10 mb-20 text-center text-4xl lg:mt-0 lg:text-6xl">
             Aktualności
           </h1>
           <NewsContainer />
