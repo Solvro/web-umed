8b37d937c7801c57c9ac2b90ef46a83cb282491e
TeraWattHour <65347730+TeraWattHour@users.noreply.github.com>
2025-07-24 00:08:18 +0200

    feat: new design of the about us section

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -6,7 +6,7 @@ import { ContactForm } from "./form";
 
 export function Contact() {
   return (
-    <section>
+    <section id="kontakt">
       <div className="mx-auto max-w-screen-xl px-4 py-10">
         <h2 className="mb-8 text-center text-3xl font-medium">Kontakt</h2>
         <div className="grid gap-x-6 gap-y-12 lg:grid-cols-2">
